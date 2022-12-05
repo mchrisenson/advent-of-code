7331a1a18559ad1993d240981802eb3cab1078f9
Mike Christenson <michael.christenosn@gmail.com>
2022-12-05 15:50:21 -0800

    day 5

--- src/main/java/_2022/_05/SupplyStacks.java
+++ src/main/java/_2022/_05/SupplyStacks.java
@@ -10,30 +10,11 @@ import java.util.stream.Collectors;
  * Advent of Code <a href="https://adventofcode.com/2022/day/5">Day 5: Supply Stacks</a>
  */
 public class SupplyStacks {
-    static class PartOne {
-        static String rearrange(List<Deque<Character>> crates, List<Step> steps) {
-            for (Step step : steps) {
-                Deque<Character> from = crates.get(step.from), to = crates.get(step.to);
-                for (int i = 0; i < step.move; ++i)
-                    to.push(from.pop());
-            }
-            return crates.stream().map(c -> !c.isEmpty() ? c.peek().toString() : "").collect(Collectors.joining());
-        }
-    }
+    static String rearrange(List<Deque<Character>> crates, List<Step> steps, StepFunction f) {
+        for (Step step : steps)
+            f.execute(step, crates.get(step.from), crates.get(step.to));
 
-    static class PartTwo {
-        static String rearrange(List<Deque<Character>> crates, List<Step> steps) {
-            Deque<Character> between = new ArrayDeque<>();
-
-            for (Step step : steps) {
-                Deque<Character> from = crates.get(step.from), to = crates.get(step.to);
-                for (int i = 0; i < step.move; ++i)
-                    between.push(from.pop());
-                while (!between.isEmpty())
-                    to.push(between.pop());
-            }
-            return crates.stream().map(c -> !c.isEmpty() ? c.peek().toString() : "").collect(Collectors.joining());
-        }
+        return crates.stream().map(c -> !c.isEmpty() ? c.peek().toString() : "").collect(Collectors.joining());
     }
 
     static List<Deque<Character>> preprocessCrates(String file) throws IOException {
@@ -52,9 +33,8 @@ public class SupplyStacks {
             for (int i = 0; i <= size; ++i)
                 crates.add(new ArrayDeque<>());
 
-            while (!lineStack.isEmpty()) {
-                String row = lineStack.pop();
-                for (int i = 1, offset = 1; i <= size; ++i, offset +=4) {
+            for (String row : lineStack) {
+                for (int i = 1, offset = 1; i <= size; ++i, offset += 4) {
                     char c = row.charAt(offset);
                     if (c != ' ') crates.get(i).push(c);
                 }
@@ -69,30 +49,42 @@ public class SupplyStacks {
 
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
-            while ((line = reader.readLine()) != null && line.length() != 0);
+            while ((line = reader.readLine()) != null && line.length() != 0) ;
             while ((line = reader.readLine()) != null) {
                 String[] split = line.split("move\\s|\\sfrom\\s|\\sto\\s");
-                steps.add(new Step(
-                        Integer.parseInt(split[1]),
-                        Integer.parseInt(split[2]),
-                        Integer.parseInt(split[3])
-                ));
+                steps.add(new Step(Integer.parseInt(split[1]), Integer.parseInt(split[2]), Integer.parseInt(split[3])));
             }
         }
         return steps;
     }
 
-    record Step(int move, int from, int to){}
+    record Step(int move, int from, int to) {
+    }
+
+    interface StepFunction {
+        void execute(Step step, Deque<Character> from, Deque<Character> to);
+    }
 
     public static void main(String[] args) throws IOException {
         String file = "src/main/resources/_2022/_05/input.txt";
         List<Deque<Character>> crates = preprocessCrates(file);
         List<Step> steps = preprocessSteps(file);
 
-        System.out.println("Part One: " + PartOne.rearrange(crates, steps));
+        System.out.println("Part One: " + rearrange(crates, steps, (step, from, to) -> {
+            for (int i = 0; i < step.move; ++i)
+                to.push(from.pop());
+        }));
 
         crates = preprocessCrates(file);
 
-        System.out.println("Part Two: " + PartTwo.rearrange(crates, steps));
+        System.out.println("Part Two: " + rearrange(crates, steps, (step, from, to) -> {
+            Deque<Character> between = new ArrayDeque<>();
+
+            for (int i = 0; i < step.move; ++i)
+                between.push(from.pop());
+
+            for (Character crate : between)
+                to.push(crate);
+        }));
     }
 }
